3ead5f5c15ef0f2107ebfe06acb44373a2934350
Ben Woodfield <raser.13@hotmail.com>
2016-10-28 18:22:09 +0000

    Outputs wordlist results to a text file
    This is an upgrade of my previous attempts
    Basically this puts the output into a txt Text document
    It also has a bit of User input - user selects how many characters, and the name of the output text file
    
    txt output file will be saved into the same directory as the script is saved in

---
+++ wordlist_to_txt_generator.py
@@ -0,0 +1,50 @@
+# Ben Woodfield
+# Another Wordlist Generator
+# This one outputs the wordlist niceley into a text file (txt)
+# The output file will be in the same directory as THIS script
+
+import time
+print ("---------------------------------------------------");
+print ("Welcome to BruteForce List Generator!");
+print ("---------------------------------------------------");
+print ("File output can be long, sometimes in x100Mbs!");
+print ("so, have patience fgt,");
+print (" ");
+print ("Press ^C to exit");
+print ("---------------------------------------------------");
+length=int(raw_input("Enter the maximum of characters: "))
+name=raw_input("Enter destination file name with extension (.txt): ")
+tic = time.clock()
+print ("---------------------------------------------------");
+print ("Running, Please Wait!");
+print ("---------------------------------------------------");
+lista=[0 for x in xrange(length)]
+x=length-1
+string="abcdefghijklnmopqrstuvwxyz1234567890"
+list_of_results=[]
+file1=file(name,"w")
+while(x>-1):
+    result=""
+    if lista[x]==len(string)-1:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]=0
+        x-=1
+    elif x==length-1:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+    else:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+        if x>0:
+            x+=1
+        else:
+            x=length-1
+    file1.write(result+"\n")
+toc = time.clock()
+ttn = toc - tic
+print ("Done! in "+str(ttn)+" seconds.");
+print ("Please check "+str(name)+" in your directory");
+print ("---------------------------------------------------");
